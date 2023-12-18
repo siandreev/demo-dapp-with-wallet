4f87c2225f5325068c7ccbc71c153e7dc7ce54ce
siandreev <andreev.sergey.i@yandex.ru>
2023-12-18 19:30:39 +0100

    fix: added tonkeeper desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 import './App.scss'
-import {THEME, TonConnectUIProvider} from "@tonconnect/ui-react";
+import {THEME, TonConnect, TonConnectUIProvider} from "@tonconnect/ui-react";
 import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
 import {Footer} from "./components/Footer/Footer";
 import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
 
+const connector = new TonConnect({ manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json', walletsListSource: 'https://raw.githubusercontent.com/ton-blockchain/wallets-list/tonkeeper-deeplink/wallets-v2.json' });
+
 function App() {
   return (
       <TonConnectUIProvider
-          manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+          connector={connector}
           uiPreferences={{ theme: THEME.DARK }}
-          walletsListConfiguration={{
-            includeWallets: [
-              {
-                appName: "tonwallet",
-                name: "TON Wallet",
-                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
-                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
-                universalLink: "https://wallet.ton.org/ton-connect",
-                jsBridgeKey: "tonwallet",
-                bridgeUrl: "https://bridge.tonapi.io/bridge",
-                platforms: ["chrome", "android"]
-              }
-            ]
-          }}
           actionsConfiguration={{
               twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
           }}
